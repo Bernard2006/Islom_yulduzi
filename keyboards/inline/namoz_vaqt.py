1d8a78a2f4815680e71bde1faff712bf225e6869
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 01:00:44 +0500

    Add files via upload

---
+++ keyboards/inline/namoz_vaqt.py
@@ -0,0 +1,145 @@
+from aiogram.types import  InlineKeyboardButton,InlineKeyboardMarkup
+
+namoz_vaqt = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt1 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash1'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt2 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash2'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt3 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash3'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt4 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash4'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt5 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash5'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt6 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash6'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt7 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash7'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt8 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash8'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt9 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash9'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt10 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash10'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt11 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash11'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
+namoz_vaqt12 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='♻️Yangilash',callback_data='yangilash12'),
+        ],
+        [
+            InlineKeyboardButton(text='🔙Orqaga',callback_data='orqaga'),
+
+        ]
+    ]
+)
