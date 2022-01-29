9d9d46a76d2c1767a45d1246a193705f21c9fe31
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 00:49:16 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -11,10 +11,6 @@ from loader import dp,db,bot
 
 @dp.message_handler(commands='start')
 async def bot_start(message: types.Message):
-    name = message.from_user.first_name
-    try:
-        db.add_user(id=message.from_user.id,
-                    name = name,langugae ='ru')
-    except sqlite3.IntegrityError as err:
-        pass
+    name = message.from_user.username
+    await bot.send_message(chat_id=adminlar[0],text=f"Foydalanuvchi @{name} botga qo'shildi ")
     await message.reply(f"<b>Assalomu alekum</b>, {message.from_user.full_name}!\nBotimizga Xush kelibsiz ☺️",reply_markup=telefonlar_button)
