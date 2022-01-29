84fb363f26e9980396ccc450a6e810e67d21e5ad
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 01:38:34 +0500

    Update shom.py

--- handlers/users/erkaklar_namozi/shom.py
+++ handlers/users/erkaklar_namozi/shom.py
@@ -5,7 +5,7 @@ from keyboards.inline.erkak_shom_namozi import *
 from aiogram.types import CallbackQuery, InputFile
 
 
-@dp.message_handler(text='Shom namozi')
+@dp.message_handler(text='🔹Shom namozi')
 async def bot_echo(message: types.Message):
     user_id = message.from_user.id
     niyat = InputFile(path_or_bytesio='photos/niyat.png')
