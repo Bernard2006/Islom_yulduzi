463f7e6c1c69bb14f807d31d33c8c582202ca865
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 01:32:40 +0500

    Update ayol_bomdod.py

--- keyboards/default/ayol_bomdod.py
+++ keyboards/default/ayol_bomdod.py
@@ -3,15 +3,15 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 namoz_vaqtlari_ayol = ReplyKeyboardMarkup(resize_keyboard=True,
     keyboard=[
         [
-            KeyboardButton(text='Bomdod namozi ayol'),
-            KeyboardButton(text='Asr namozi ayol')
+            KeyboardButton(text='🔹Bomdod namozi ayol'),
+            KeyboardButton(text='🔹Asr namozi ayol')
         ],
         [
-            KeyboardButton(text='Peshin namozi ayol')
+            KeyboardButton(text='🔹Peshin namozi ayol')
         ],
         [
-            KeyboardButton(text="Shom namozi ayol"),
-            KeyboardButton(text="Xufton namozi ayol")
+            KeyboardButton(text="🔹Shom namozi ayol"),
+            KeyboardButton(text="🔹Xufton namozi ayol")
         ],
         [
             KeyboardButton(text="Menuga qaytish🔙")
