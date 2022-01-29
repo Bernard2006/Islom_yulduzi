e64815932fa86ef8fa9a213710f484a2c7062c5a
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 00:55:35 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from utils.db_api.baza import Database
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
