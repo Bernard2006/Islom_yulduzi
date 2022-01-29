4997a5c537ed3ab2c578ff2bc090a1763aa59e71
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 00:51:10 +0500

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -4,10 +4,7 @@ from loader import dp,db
 
 
 # Echo bot
-@dp.message_handler(commands='baza')
-async def bot(message: types.Message):
-    db.create_table_users()
-    await message.answer(text='baza yaratildi')
+
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
     await message.answer(message.text)
