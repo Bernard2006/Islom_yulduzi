05141f4f43a69569b69e0c30ae61591c9bd7e365
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 01:37:50 +0500

    Update bomdod.py

--- handlers/users/erkaklar_namozi/bomdod.py
+++ handlers/users/erkaklar_namozi/bomdod.py
@@ -6,7 +6,7 @@ from keyboards.inline.erkak_bomdod import *
 from aiogram.types import CallbackQuery, InputFile
 
 
-@dp.message_handler(text='Bomdod namozi')
+@dp.message_handler(text='🔹Bomdod namozi')
 async def bot_echo(message: types.Message):
     user_id = message.from_user.id
     niyat = InputFile(path_or_bytesio='photos/niyat.png')
