ca1bb7fec3ef26b1734881c646fa3198a2e1779c
Bernard2006 <98524519+Bernard2006@users.noreply.github.com>
2022-01-30 21:50:56 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,8 +12,9 @@ from loader import dp,bot
 @dp.message_handler(commands='start')
 async def bot_start(message: types.Message):
     name = message.from_user.username
-    if name == None:
+        if name == None:
         name2 = message.from_user.first_name
-        await bot.send_message(chat_id=adminlar[0], text=f"Foydalanuvchi @{name2} botga qo'shildi ")
-    await bot.send_message(chat_id=adminlar[0],text=f"Foydalanuvchi @{name} botga qo'shildi ")
+        await bot.send_message(chat_id=adminlar[0], text=f"Foydalanuvchi {name2} botga qo'shildi ")
+    else:
+        await bot.send_message(chat_id=adminlar[0], text=f"Foydalanuvchi @{name} botga qo'shildi ")
     await message.reply(f"<b>Assalomu alekum</b>, {message.from_user.full_name}!\nBotimizga Xush kelibsiz ☺️",reply_markup=telefonlar_button)
